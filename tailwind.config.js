5321f0d93aa63597d37db072fbdaf14a8f39c6a1
Aatmane Oulmouden <aatmaneoulmouden21@gmail.com>
2024-05-31 21:12:20 +0100

    Initialize the project, install tailwind, and add setup the app configuration

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    container: {
+      center: true,
+    },
+    fontFamily: {
+      "rubik": ["Rubik", "sans-serif"],
+    },
+    extend: {
+      colors: {
+        "primary": "#2E8BC0",
+        "text": "#1A3A57",
+        "soft-blue-white": "#F3F9FB",
+      }
+    },
+  },
+  plugins: [],
+}
+
