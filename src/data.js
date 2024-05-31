b41ea121ec380f69cf568fc3d3a5a04af5769233
Aatmane Oulmouden <aatmaneoulmouden21@gmail.com>
2024-05-31 21:16:39 +0100

    Add data file and clean the app

---
+++ src/data.js
@@ -0,0 +1,47 @@
+export default ([
+    {
+        title: "Mount Fuji",
+        location: "Japan",
+        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+        startDate: "12 Jan, 2021",
+        endDate: "24 Jan, 2021",
+        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+        imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
+    },
+    {
+        title: "Eiffel Tower",
+        location: "France",
+        googleMapsUrl: "https://goo.gl/maps/RsNoHz6YwRxkivQe7",
+        startDate: "05 Mar, 2021",
+        endDate: "14 Mar, 2021",
+        description: "The Eiffel Tower is one of the most recognizable structures in the world, located in Paris, France. It was completed in 1889 and is a global cultural icon of France.",
+        imageUrl: "https://source.unsplash.com/JmuyB_LibRo"
+    },
+    {
+        title: "Grand Canyon",
+        location: "USA",
+        googleMapsUrl: "https://goo.gl/maps/uTUzdrKkwZp",
+        startDate: "20 Apr, 2021",
+        endDate: "28 Apr, 2021",
+        description: "The Grand Canyon is a steep-sided canyon carved by the Colorado River in Arizona, United States. It is considered one of the Seven Natural Wonders of the World.",
+        imageUrl: "https://source.unsplash.com/Q1p7bh3SHj8"
+    },
+    {
+        title: "Great Wall of China",
+        location: "China",
+        googleMapsUrl: "https://goo.gl/maps/8QhQ8jAzhh32",
+        startDate: "15 May, 2021",
+        endDate: "25 May, 2021",
+        description: "The Great Wall of China is a series of fortifications made of stone, brick, tamped earth, wood, and other materials, built along the northern borders of China to protect against various nomadic groups.",
+        imageUrl: "https://source.unsplash.com/4hMET7vYTAQ"
+    },
+    {
+        title: "Machu Picchu",
+        location: "Peru",
+        googleMapsUrl: "https://goo.gl/maps/Ce7eT7rCw9P2",
+        startDate: "10 Jun, 2021",
+        endDate: "18 Jun, 2021",
+        description: "Machu Picchu is a 15th-century Inca citadel located in the Eastern Cordillera of southern Peru on a mountain ridge 2,430 meters (7,970 ft) above sea level.",
+        imageUrl: "https://source.unsplash.com/6dX5TPPM34M"
+    }
+]);
